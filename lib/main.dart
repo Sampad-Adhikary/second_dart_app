5f7a837fb03c7730f51b93c6f7bb99899cfa1dfd
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-01-17 14:52:43 +0530

    second problem solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,138 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(
+    MaterialApp(
+      home: Scaffold(
         body: SafeArea(
-            child: Container(
-                width: 400,
-                height: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      color: Colors.red,
-                      height: double.infinity,
-                      child: Text("I am red"),
+          child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: double.infinity,
+                    // height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(color: Colors.yellow, width: 5),
+                        borderRadius: BorderRadius.circular(35)),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              alignment: Alignment.topLeft,
+                              padding: EdgeInsets.all(10),
+                              child: Text("23/05/2023"),
+                            ),
+                            Container(
+                              // width: 20,
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.all(10),
+                              child: Text("Monday"),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(10),
+                          child: Text("Go to Gym"),
+                        ),
+                      ],
                     ),
-                    Container(
-                      color: Colors.blue,
-                      height: double.infinity,
-                      child: Text("I am blue"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: double.infinity,
+                    // height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(color: Colors.yellow, width: 5),
+                        borderRadius: BorderRadius.circular(35)),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              alignment: Alignment.topLeft,
+                              padding: EdgeInsets.all(10),
+                              child: Text("23/05/2023"),
+                            ),
+                            Container(
+                              // width: 20,
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.all(10),
+                              child: Text("Monday"),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(10),
+                          child: Text("Go to Gym"),
+                        ),
+                      ],
                     ),
-                    Container(
-                      color: Colors.green,
-                      height: double.infinity,
-                      child: Text("I am green"),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: double.infinity,
+                    // height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(color: Colors.yellow, width: 5),
+                        borderRadius: BorderRadius.circular(35)),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              alignment: Alignment.topLeft,
+                              padding: EdgeInsets.all(10),
+                              child: Text("23/05/2023"),
+                            ),
+                            Container(
+                              // width: 20,
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.all(10),
+                              child: Text("Monday"),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          padding: EdgeInsets.all(10),
+                          child: Text("Go to Gym"),
+                        ),
+                      ],
                     ),
-                  ],
-                )))),
-  ));
-  // debugShowCheckedModeBanner: false,
+                  ),
+                ],
+              )),
+        ),
+      ),
+      // debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
+List<Widget> myResult = [];
+List<Color> MyContainerColors = [Colors.brown, Colors.red, Colors.blue];
 
-//IMAGES: 
-// Asset Images: makes the app size big
-// FIle Images: Images stored in device 
-// Network Images: Images stored in server
+List<Widget> getMyWidget() {
+  for (var i = 0; i < MyContainerColors.length; i++) {
+    myResult.add(Container(
+      width: 40,
+      height: 40,
+      color: MyContainerColors[i],
+      child: Center(child: Text("${i + 1}")),
+    ));
+  }
+  return myResult;
+}
